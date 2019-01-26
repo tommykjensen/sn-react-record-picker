2714b2dd7c9a8140d1c2df4ca54e726b1bd2354a
Tommy Jensen <tommykjensen@gmail.com>
2019-01-26 22:02:54 +0100

    Added more fields

--- src/App.js
+++ src/App.js
@@ -27,14 +27,18 @@ export default class ReferenceField extends React.Component {
     let query = new RegExp(search, 'gi')
     const fieldVal = option.data['name'].display_value
     const secondaryFieldVal = option.data['email'].display_value
+    const userNamedVal = option.data['user_name'].display_value
+    const deparmentNamedVal = option.data['department.name'].display_value
 
-    if (fieldVal.match(query) || secondaryFieldVal.match(query)) return true
+    if (fieldVal.match(query) || secondaryFieldVal.match(query) || userNamedVal.match(query) || deparmentNamedVal.match(query) ) return true
   }
 
   formatOptionLabel = (option) => (
     <DropdownItem
       label={option['name'].display_value}
       secondaryLabel={option['email'].display_value}
+      userName={option['user_name'].display_value}
+      departmentName={option['department.name'].display_value}
     />
   )
 
@@ -60,19 +64,19 @@ export default class ReferenceField extends React.Component {
 
   buildUrl = (searchQuery, offset) => {
     const url = ['/api/now/table/sys_user?sysparm_query=']
-    const responseFields = ['sys_id', 'name', 'email']
+    const responseFields = ['sys_id', 'name', 'email', 'user_name', 'department.name']
 
     if (searchQuery) {
-      url.push(`nameLIKE${searchQuery}^ORemailLIKE${searchQuery} `)
+      url.push(`nameLIKE${searchQuery}^ORemailLIKE${searchQuery}^ORuser_nameLIKE${searchQuery}^ORdepartment.nameLIKE${searchQuery}`)
     }
 
-    url.push(`^nameISNOTEMPTY`)
+    // url.push(`^nameISNOTEMPTY`)
     url.push(`^ORDERBYname`)
     url.push('&sysparm_fields=' + responseFields.join(','))
     url.push('&sysparm_limit=' + 20)
     url.push('&sysparm_offset=' + offset)
     url.push('&sysparm_display_value=all')
-
+console.log(url.join(''));
     return url.join('')
   }
 }
@@ -86,8 +90,20 @@ const SingleValue = ({ children, ...props }) => (
 class DropdownItem extends React.PureComponent {
   render = () => (
     <div className="dropdownItem">
-      <span>{this.props.label}</span>
-      <span className="secondaryLabel">{this.props.secondaryLabel}</span>
+      
+      <table>
+        <tr>
+          <td>
+          <span>{this.props.label}</span>
+          </td>
+          <td></td>
+        </tr>
+        <tr>
+          <td><span className="">{this.props.departmentName}</span></td>
+          <td><span className="">{this.props.userName}</span></td>
+        </tr>
+      </table>
+      
     </div>
   )
 }
